835136655211741c457ce35f4fc891d97332b6d3
Daniel Worthington <daniel.worthington96@outlook.com>
2024-09-23 14:51:21 +0100

    enhancement: add user model

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 
-const exampleRouter = require("./middlewareExample");
+// const exampleRouter = require("./middlewareExample");
+const User = require("./users/model");
 
 const port = process.env.PORT || 5003;
 
@@ -10,12 +11,17 @@ const app = express();
 //Using this allows us to access and use json data
 app.use(express.json());
 
-app.use(exampleRouter);
+const syncTables = () => {
+    User.sync();
+}
+
+// app.use(exampleRouter);
 
 app.use("/health", (req, res) => {
     res.status(200).json({message: "API is healthy"});
 });
 
 app.listen(port, () => {
+    syncTables();
     console.log(`Server is listening on ${port}`);
 });
